be762ddb8ad4d72eaff2228d745bc5407ea85f78
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2024-05-27 16:33:47 +0530

    footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
 import Banner from "@/app/(home)/components/Banner";
 import BookLists from "./(home)/components/BookLists";
+import { Book } from "@/types";
 
 export default async function Home() {
-  const response = await fetch("http://localhost:8080/api/books");
-  console.log("reponse coming in fetch", response);
+  let books_list: Book[] | null = null;
+  try {
+    const response = await fetch("http://localhost:8080/api/books");
+    console.log("reponse coming in fetch", response);
+    if (!response?.ok) {
+      throw new Error("error while fetching book list");
+    }
 
-  if (!response?.ok) {
-    throw new Error("error while fetching book list");
+    books_list = await response.json();
+
+    console.log("books-list", books_list);
+  } catch (error) {
+    console.error("error fetching in nbooks list", error);
   }
 
-  const books_list = await response.json();
-
-  console.log("books-list", books_list);
+  if (!books_list) {
+    throw new Error("Books cannot be fetched");
+  }
 
   return (
     <>
